51bbdfc427a1111fcf7c093da60174e0dbb744f9
abhishek-designs <akalyane980@gmail.com>
2020-10-07 14:48:51 +0530

    Minor fixes

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,18 +1,49 @@
 // Accessing toggler of menu
 const toggler = document.querySelector('.toggler');
+toggler.addEventListener('click',onChecked);
 
-var checked = false;
 
-toggler.addEventListener('click',function(){
-    if(checked == false)
+function onChecked()
+{
+    
+    if(this.checked)
     {
-        checked = true;
-        
-        // disable scroll
+        // Disable scroll
+        disableScroll();
     }
     else
     {
-        checked = false;
-        console.log('unchecked');
+        // Enable scroll
+        enableScroll();
     }
-})
+}
+
+// Control scrolling
+window.addEventListener('scroll',disableScroll);
+window.addEventListener('scroll',enableScroll);
+
+function disableScroll()
+{
+    
+    document.body.style.overflow = "hidden";
+}
+
+function enableScroll()
+{
+    document.body.style.overflow = "auto";
+}
+
+
+const menuWrap = document.querySelector('.menu-wrap');
+var links = menuWrap.querySelectorAll('.nav-links');
+
+for(let i=0; i<links.length; i++)
+{
+    var link = links[i];
+    link.addEventListener('click',disableMenu);
+}
+
+function disableMenu()
+{
+    toggler.checked = false;
+}
