ba0f42cd4c9d6032788e09f659c14ede0f93b696
abhishek-designs <akalyane980@gmail.com>
2020-10-08 17:42:38 +0530

    Minor fixes

--- dist/scripts/complex_anim.js
+++ dist/scripts/complex_anim.js
@@ -10,23 +10,42 @@ tl.to(".text", {duration: 1, y: 0, stagger: 0.2})
   .fromTo(".showcase-art .circle-art .circular-grid .circle", {opacity: 0, y: -50}, {opacity:1, y: 0, duration: 0.4, stagger: 0.1});
 
 // ScrollTrigger
+// About section
 let t1 = gsap.timeline({
     scrollTrigger: {
         trigger: '.about',
         start: 'center bottom',
+        duration: 1,
+        ease:"power1.out"
     }
 });
     
-t1.from(".about .feature", {x: -100, opacity: 0, duration: 1, stagger: 0.2} );
+t1.from(".about .feature", {x: -100, opacity: 0, stagger: 0.2})
+  .from(".about .img-contain", {x: 200}, "-=0.5")
+  .fromTo(".about .circular-grid .circle", {opacity: 0, y: -50}, {opacity: 1, y: 0, duration:0.4, stagger: 0.1})
 
-let t2 = gsap.timeline({
+// Gallery section
+const t2 = gsap.timeline({
     scrollTrigger: {
         trigger: ".gallery",
         start: 'center bottom',
+        ease:"power1.out"
     }
 });
 
 t2.from(".gallery .img", {scale: 0, duration: 1});
+
+// Trainer section
+const t3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".trainers",
+        start: "center bottom",
+        duration: 1,
+        ease:"power1.out"
+    }
+});
+
+t3.from(".trainers .trainer-card", {opacity: 0, x: -50, stagger: 0.2})
 
 // Navbar shrink animation
 // Accessing toggler
